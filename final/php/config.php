3dcb1d45b1544ac5fe125d48fd0a7160604405c1
Andrew Nieves <106421892+ajnieves1@users.noreply.github.com>
2023-05-29 17:44:58 -0400

    Delete final directory

--- final/php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("SERVER", "cis-linux2.temple.edu");
-	define("USER", "tun29995");
-	define("PASSWORD", "aichai4Aig");
-	define("DATABASE", "sp23_1052_tun29995");
-?>
--- final/php/forgetView.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION["RegState"] = 5;
-header("location:../index.php");
-exit();
-?>
